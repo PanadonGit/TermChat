6be4c4688be0c945a5752e892700b749c4e6c908
PanadonGit <boom2222tv@gmail.com>
2024-09-10 00:52:39 +0700

    CLIENT: Add Websocket Client (for testing)

--- Client/test_socketio.py
+++ Client/test_socketio.py
@@ -4,11 +4,15 @@ import asyncio
 sio = socketio.AsyncClient()
 
 async def connect():
-    await sio.connect('http://localhost:3000', transports=['websocket'])
+    await sio.connect('http://localhost:3000', transports=['websocket'],headers={"auth":"1234"})
 
     @sio.on('message')
     async def on_message(data):
-        print(f"Message received: {data}")
+        print(f"Normal Message received: {data}")
+
+    @sio.on('notification')
+    async def on_message(data):
+        print(f"Notification received: {data}")
 
     await sio.wait()
 
